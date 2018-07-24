c2e0c53be7fa41c5c9e9ce66d454a935512c1a64
hanam0822 <hanam0822@gmail.com>
2018-07-24 19:01:09 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz (value)
+  if value % 15 == 0 
+  puts "fizzbuzz"
+
+elsif value % 3 == 0
+  puts "fizz"
+  
+elsif value % 5 == 0
+  puts "buzz"
+  
+else 
+  puts nil
+  
+  end 
+end
+
+
