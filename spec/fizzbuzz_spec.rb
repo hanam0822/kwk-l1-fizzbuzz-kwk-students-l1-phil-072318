8ddd33b77a1b946fdd4fc9fe5267eaa617084be9
hanam0822 <hanam0822@gmail.com>
2018-07-24 18:53:44 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,15 +1,15 @@
 puts "Enter a Value"
  value = gets.chomp.to_i
+
+if value % 15 == 0 
+  puts "fizzbuzz"
+
  if value % 3 == 0
   puts "fizz"
   
 elsif
 if value % 5 == 0
   puts "buzz"
-  
-elsif 
-if value % 15 == 0 
-  puts "fizzbuzz"
   
 else 
   puts nil
