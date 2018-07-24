4fc03b33d4bbb6645d21d964bffae0b38cc7edec
hanam0822 <hanam0822@gmail.com>
2018-07-24 19:07:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,8 @@ def fizzbuzz (value)
   if value % 3 == 0 # if the number value is divisible by 3
   return "Fizz"
   
-elsif value % 15 == 0
-  return "FizzBuzz"
+elsif value % 5 == 0
+  return "Buzz"
   
 else 
   return nil
