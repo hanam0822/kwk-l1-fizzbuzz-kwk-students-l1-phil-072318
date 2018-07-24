6515be3f58278a3b428782053353257f1ab27aa9
hanam0822 <hanam0822@gmail.com>
2018-07-24 19:05:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,9 @@
 def fizzbuzz (value)
+  if value % 5 == 0 && value  % 3 == 0
+  return "FizzBuzz"
+ end 
   if value % 15 == 0 
-  return "fizzbuzz"
+  return "FizzBuzz"
 
 elsif value % 3 == 0
   return "fizz"
